2e0acc83519df04350ae4cbd9416ec22e90c8dca
Ranper <634539503@qq.com>
2022-11-05 00:00:30 +0800

    2-7 增加自定义配置， 可以给设置默认值。 通过@Value注解，可以读取配置文件的中的变量，通过设置：字符，可以设置默认值。

--- src/main/java/com/peiran/wiki/controller/TestController.java
+++ src/main/java/com/peiran/wiki/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.peiran.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController  // 返回字符串,json
 //@Controller // 返回一个页面
 public class TestController {
 
+    @Value("${test.hello:NOT_CONFIG}")
+    private  String test_hello;
 //    @RequestMapping("/hello")  //  支持所有的请求方式
     @RequestMapping("/hello")  //  支持所有的请求方式
 //    @GetMapping()
@@ -15,6 +18,6 @@ public class TestController {
 //    @DeleteMapping
 //    @RequestMapping(value = "/user/1", method = RequestMethod.GET)
     public String hello(){
-        return "hello, world!  --by peiran";
+        return "hello, world!  --by peiran" + test_hello;
     }
 }
