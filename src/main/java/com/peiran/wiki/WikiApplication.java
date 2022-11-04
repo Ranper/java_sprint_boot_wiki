7134e3f9230f1d4dac601070cda4abbda146c1a9
Ranper <634539503@qq.com>
2022-11-04 21:29:05 +0800

    --test push

--- src/main/java/com/peiran/wiki/WikiApplication.java
+++ src/main/java/com/peiran/wiki/WikiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WikiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		System.out.println("hello world! -- by peiran");
 		SpringApplication.run(WikiApplication.class, args);
 	}
 
