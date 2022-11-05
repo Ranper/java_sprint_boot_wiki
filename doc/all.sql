35a0fdbcb00d68d8166c66e5b355e7aefc3c7625
Ranper <634539503@qq.com>
2022-11-05 16:36:19 +0800

    3-5 集成mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,7 +1,14 @@
 --drop table if exists `test`;
---create table if not exists `test` (
+--create table  `test` (
 --    `id` bigint not null ,
 --    `name` varchar(50) ,
 --    `password` varchar(50) ,
 --    primary key (`id`)
 --) engine=innodb default charset=utf8mb4;
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null ,
+    `name` varchar(50) ,
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4;
