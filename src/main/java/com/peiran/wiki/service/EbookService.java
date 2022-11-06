4a911aa96d60a4a216ba856bb4c73641c66a6a0b
Ranper <634539503@qq.com>
2022-11-06 17:15:23 +0800

    3-8 制作copyutil 封装beanutils。

--- src/main/java/com/peiran/wiki/service/EbookService.java
+++ src/main/java/com/peiran/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.peiran.wiki.domain.EbookExample;
 import com.peiran.wiki.mapper.EbookMapper;
 import com.peiran.wiki.req.EbookReq;
 import com.peiran.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.peiran.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,12 +25,14 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");  // 对like的封装
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook, ebookResp);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> resp = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return resp;
     }
 }
